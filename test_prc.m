cd8bf51cfebe75102750fbb6d5f1e6077b2673b9
Elise Beaudin <beaudin@jason.ouranos.ca>
2014-07-17 17:00:26 -0400

    construction du code meteo

--- test_prc.m
+++ test_prc.m
@@ -1,17 +1,45 @@
 %%% TEST DE LA FONCTION PERCTILE AVEC LA TEMP DE MANIC 2 ET 5
 
-an = 1950; % in [1950,2013]
-   
-    hold on
-for an=1980:2000
-     figure
-     percentiles = zeros(100,1);
-    for p=1:100
-        P = linspace(1,100);
-        matrice = [[1:366]' METEO(:,an-1949)];
-        percentiles(p,an) = perctile(matrice(:,2),p);
-        srtmat = sort(matrice,2);
-    end
-    plot(1:100,percentiles)
-    xlim([1 100])
+
+
+%%% M?THODE 1, pour size(METEO) quelconque
+figure
+nb_quant=12;
+P = linspace(1,100,nb_quant);
+percentiles = zeros(nb_quant,64);
+
+tic
+for an=1950:2013
+    matrice = [[1:366]' METEO(:,an-1949)];
+    %for i=round(P)
+    percentiles(1:nb_quant,an-1949) = perctile(matrice(:,2),P);
+    srtmat = sortrows(matrice,2);
+    plot(1:nb_quant,percentiles)
+    xlim([1 nb_quant])
 end
+toc
+
+
+%%% M?THODE 2, pour size(METEO) = 366 2
+figure
+nb_quant=12;
+P = linspace(1,100,nb_quant);
+percentiles = zeros(nb_quant,64);
+matrice = [[1:366]' METEO(:,i)];
+srtmat = sortrows(matrice,2);
+
+tic
+for i=1:nb_quant
+    a =(i-1)*round(366/nb_quant)+1;
+    b = i*round(366/nb_quant);
+    % IL FAUT FAIRE EN SORTE QUE QUANTILES PRENNENT LES VALEURS DE a:366,
+    % PUIS LES RESTES SERONT DES NaN.
+    if b>366, quantiles(:,i) = srtmat([a:366 ,2]);
+    else quantiles(:,i) = srtmat(a:b,2);
+    end
+    %percentiles(1:nb_quant,i) = find(perctile(matrice(:,2),89));
+    %plot(1:nb_quant,percentiles)
+    %xlim([1 nb_quant])
+end
+toc
+
